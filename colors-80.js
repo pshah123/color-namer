a48392918e402006df08fd244eb3e91bcbb46a41
Priansh Shah <shah@priansh.com>
2018-11-05 13:18:01 -0500

    fix everythign, switch to x11 and rgb distances

---
+++ colors-80.js
@@ -0,0 +1,365 @@
+module.exports = [{
+	"name": "Acid green",
+	"hsv": [65, 86, 75],
+	"distance": 6382.205986137022
+}, {
+	"name": "Aero blue",
+	"hsv": [151, 21, 100],
+	"distance": 7884.781080573666
+}, {
+	"name": "Alien Armpit",
+	"hsv": [85, 99, 87],
+	"distance": 11385.356370607366
+}, {
+	"name": "Apple",
+	"hsv": [104, 61, 71],
+	"distance": 4797.187774136721
+}, {
+	"name": "Aquamarine",
+	"hsv": [160, 50, 100],
+	"distance": 10602.830660057123
+}, {
+	"name": "Avocado",
+	"hsv": [81, 98, 51],
+	"distance": 3109.912028640649
+}, {
+	"name": "Bayside",
+	"hsv": [174, 53, 79],
+	"distance": 6812.395220605392
+}, {
+	"name": "Bitter lemon",
+	"hsv": [66, 94, 88],
+	"distance": 10391.249519841493
+}, {
+	"name": "Bleu de France",
+	"hsv": [210, 79, 91],
+	"distance": 12827.84920242087
+}, {
+	"name": "Blue",
+	"hsv": [240, 100, 100],
+	"distance": 19309.919402011856
+}, {
+	"name": "Blue (Crayola)",
+	"hsv": [217, 88, 100],
+	"distance": 17207.88594667458
+}, {
+	"name": "Blue (RYB)",
+	"hsv": [224, 99, 100],
+	"distance": 19461.11760678346
+}, {
+	"name": "Blue sapphire",
+	"hsv": [197, 86, 50],
+	"distance": 4324.596229043301
+}, {
+	"name": "Blue-violet",
+	"hsv": [271, 81, 89],
+	"distance": 11328.201954223567
+}, {
+	"name": "Blueberry",
+	"hsv": [220, 68, 97],
+	"distance": 12703.65598428861
+}, {
+	"name": "Button blue",
+	"hsv": [203, 83, 93],
+	"distance": 14080.676899999999
+}, {
+	"name": "Byzantine",
+	"hsv": [311, 73, 74],
+	"distance": 5896.080426752566
+}, {
+	"name": "Caribbean green",
+	"hsv": [165, 100, 80],
+	"distance": 12320.902784844435
+}, {
+	"name": "Citron",
+	"hsv": [64, 82, 66],
+	"distance": 4385.542676411419
+}, {
+	"name": "Cyber grape",
+	"hsv": [263, 47, 49],
+	"distance": 2069.7427
+}, {
+	"name": "Dark cerulean",
+	"hsv": [209, 94, 49],
+	"distance": 4616.965276598476
+}, {
+	"name": "Dark lavender",
+	"hsv": [270, 47, 59],
+	"distance": 3029.355580501051
+}, {
+	"name": "Dark orchid",
+	"hsv": [280, 75, 80],
+	"distance": 8139.621723706418
+}, {
+	"name": "Dark pastel green",
+	"hsv": [138, 98, 75],
+	"distance": 9826.594489593808
+}, {
+	"name": "Dark yellow",
+	"hsv": [52, 92, 61],
+	"distance": 3911.3256159451994
+}, {
+	"name": "Deep magenta",
+	"hsv": [300, 100, 80],
+	"distance": 10291.506216469159
+}, {
+	"name": "Deep violet",
+	"hsv": [270, 100, 40],
+	"distance": 2759.066954491238
+}, {
+	"name": "Electric blue",
+	"hsv": [183, 51, 100],
+	"distance": 11020.453743643067
+}, {
+	"name": "Electric ultramarine",
+	"hsv": [255, 100, 100],
+	"distance": 18799.787793207935
+}, {
+	"name": "Eucalyptus",
+	"hsv": [161, 68, 84],
+	"distance": 9129.480296183774
+}, {
+	"name": "Flirt",
+	"hsv": [320, 100, 64],
+	"distance": 5689.259271504732
+}, {
+	"name": "Fuchsia",
+	"hsv": [300, 100, 100],
+	"distance": 16743.551870586452
+}, {
+	"name": "Gamboge orange (brown)",
+	"hsv": [40, 100, 60],
+	"distance": 4043.617004225034
+}, {
+	"name": "Grape",
+	"hsv": [272, 73, 66],
+	"distance": 5398.0167585175
+}, {
+	"name": "Green (RYB)",
+	"hsv": [95, 72, 69],
+	"distance": 4938.413235858339
+}, {
+	"name": "Han purple",
+	"hsv": [255, 90, 98],
+	"distance": 16048.485018409401
+}, {
+	"name": "Harlequin green",
+	"hsv": [105, 88, 80],
+	"distance": 8846.321317744432
+}, {
+	"name": "Heliotrope",
+	"hsv": [286, 55, 100],
+	"distance": 10295.198051177453
+}, {
+	"name": "Heliotrope magenta",
+	"hsv": [295, 100, 73],
+	"distance": 8588.294748331202
+}, {
+	"name": "Iguana green",
+	"hsv": [126, 40, 74],
+	"distance": 4505.124007695166
+}, {
+	"name": "Illuminating emerald",
+	"hsv": [164, 66, 57],
+	"distance": 4024.694122231913
+}, {
+	"name": "Inchworm",
+	"hsv": [84, 61, 93],
+	"distance": 8421.825896786697
+}, {
+	"name": "Interdimensional blue",
+	"hsv": [253, 94, 80],
+	"distance": 11166.016675566167
+}, {
+	"name": "Kelly green",
+	"hsv": [101, 88, 73],
+	"distance": 7084.7290331344575
+}, {
+	"name": "Kiwi",
+	"hsv": [91, 72, 90],
+	"distance": 9045.590337776572
+}, {
+	"name": "Liberty",
+	"hsv": [236, 50, 65],
+	"distance": 4314.4930016153485
+}, {
+	"name": "Lime green",
+	"hsv": [120, 76, 80],
+	"distance": 8066.544811483439
+}, {
+	"name": "Maximum Green Yellow",
+	"hsv": [65, 65, 90],
+	"distance": 7733.519252542879
+}, {
+	"name": "May green",
+	"hsv": [112, 55, 57],
+	"distance": 2761.8920335823695
+}, {
+	"name": "Medium aquamarine",
+	"hsv": [154, 54, 87],
+	"distance": 8160.751690713436
+}, {
+	"name": "Medium sea green",
+	"hsv": [147, 66, 70],
+	"distance": 5858.035374472916
+}, {
+	"name": "Medium spring green",
+	"hsv": [157, 100, 98],
+	"distance": 18257.29698610289
+}, {
+	"name": "Menthol",
+	"hsv": [99, 35, 98],
+	"distance": 7700.467561988367
+}, {
+	"name": "Metallic blue",
+	"hsv": [214, 59, 48],
+	"distance": 2732.4011295664313
+}, {
+	"name": "Metallic Sunburst",
+	"hsv": [41, 64, 61],
+	"distance": 2619.2818104749813
+}, {
+	"name": "Middle blue",
+	"hsv": [190, 212, 230],
+	"distance": 296562.95087926916
+}, {
+	"name": "Middle green",
+	"hsv": [130, 45, 55],
+	"distance": 2452.6313474098306
+}, {
+	"name": "Middle green yellow",
+	"hsv": [72, 50, 75],
+	"distance": 4278.6638851904245
+}, {
+	"name": "Middle yellow red",
+	"hsv": [30, 128, 236],
+	"distance": 131048.43583931988
+}, {
+	"name": "Morning blue",
+	"hsv": [153, 14, 64],
+	"distance": 2644.176630280224
+}, {
+	"name": "Mustard brown",
+	"hsv": [36, 100, 80],
+	"distance": 8390.088607503012
+}, {
+	"name": "Napier green",
+	"hsv": [100, 100, 50],
+	"distance": 3469.741830244652
+}, {
+	"name": "Neon blue",
+	"hsv": [250, 98, 64],
+	"distance": 7529.687247550184
+}, {
+	"name": "Neon green",
+	"hsv": [142, 88, 61],
+	"distance": 5737.431301965276
+}, {
+	"name": "Neon lime",
+	"hsv": [111, 92, 100],
+	"distance": 15457.807786937952
+}, {
+	"name": "Olive drab",
+	"hsv": [80, 75, 56],
+	"distance": 2841.573075558804
+}, {
+	"name": "Pale violet",
+	"hsv": [270, 40, 100],
+	"distance": 9119.066954491238
+}, {
+	"name": "Pear",
+	"hsv": [66, 78, 89],
+	"distance": 8781.97639781669
+}, {
+	"name": "Pearly purple",
+	"hsv": [316, 43, 72],
+	"distance": 3840.579843623782
+}, {
+	"name": "Picton blue",
+	"hsv": [200, 70, 91],
+	"distance": 11501.535714024596
+}, {
+	"name": "Prilly blue",
+	"hsv": [131, 142, 115],
+	"distance": 37872.10740805823
+}, {
+	"name": "Prilly pink",
+	"hsv": [220, 240, 150],
+	"distance": 157081.56317464978
+}, {
+	"name": "Prilly red",
+	"hsv": [230, 240, 120],
+	"distance": 100901.60702557903
+}, {
+	"name": "Purple (Munsell)",
+	"hsv": [288, 100, 77],
+	"distance": 9910.426062300963
+}, {
+	"name": "Purple navy",
+	"hsv": [236, 39, 50],
+	"distance": 2119.5252409692093
+}, {
+	"name": "Razzle dazzle rose",
+	"hsv": [315, 80, 100],
+	"distance": 12647.80105404515
+}, {
+	"name": "Royal blue",
+	"hsv": [225, 71, 88],
+	"distance": 10754.17486886532
+}, {
+	"name": "Russian violet",
+	"hsv": [270, 70, 30],
+	"distance": 1033.0888611079
+}, {
+	"name": "Sea blue",
+	"hsv": [197, 100, 58],
+	"distance": 6847.505285686312
+}, {
+	"name": "Sea green (Crayola)",
+	"hsv": [168, 100, 100],
+	"distance": 19367.11949947771
+}, {
+	"name": "Spring Frost",
+	"hsv": [94, 84, 100],
+	"distance": 13376.869027713696
+}, {
+	"name": "Steel pink",
+	"hsv": [300, 75, 80],
+	"distance": 7559.460562351869
+}, {
+	"name": "Tiffany Blue",
+	"hsv": [178, 95, 73],
+	"distance": 9892.07226746405
+}, {
+	"name": "Turquoise",
+	"hsv": [174, 71, 88],
+	"distance": 10662.611151741696
+}, {
+	"name": "Ultramarine blue",
+	"hsv": [228, 73, 96],
+	"distance": 13117.30170606893
+}, {
+	"name": "Vegas gold",
+	"hsv": [50, 55, 77],
+	"distance": 4535.818658773359
+}, {
+	"name": "Very light blue",
+	"hsv": [240, 60, 100],
+	"distance": 12019.281081207113
+}, {
+	"name": "Very light malachite green",
+	"hsv": [135, 57, 91],
+	"distance": 8902.552430733474
+}, {
+	"name": "Violet (Caran d'Ache)",
+	"hsv": [194, 255, 96],
+	"distance": 73103.36022908201
+}, {
+	"name": "Violet (RYB)",
+	"hsv": [286, 99, 69],
+	"distance": 7796.677768362396
+}, {
+	"name": "Vivid mulberry",
+	"hsv": [288, 95, 89],
+	"distance": 12732.47432321489
+}]
--- index.js
+++ index.js
@@ -1,60 +1,51 @@
-const colors = require('./colors-30')
+const colors = require('./colors-x11')
 const assert = require('assert')
-Math.radians = degrees => degrees * Math.PI / 180;
-
-const rgb_to_hsv = (r, g, b) => {
-    var max = Math.max(r, g, b), min = Math.min(r, g, b),
-        d = max - min,
-        h,
-        s = (max === 0 ? 0 : d / max),
-        v = max / 255;
-    switch (max) {
-        case min: h = 0; break;
-        case r: h = (g - b) + d * (g < b ? 6: 0); h /= 6 * d; break;
-        case g: h = (b - r) + d * 2; h /= 6 * d; break;
-        case b: h = (r - g) + d * 4; h /= 6 * d; break;
-    }
-    return [h*360, s*100, v*100]
-}
 
 const hex_to_rgb = hex => {
     let result = /^([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return result ? [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)] : null
 }
 
-const hsvDistance = (hsv1, hsv2) => (
-    ((Math.sin(Math.radians(hsv1[0])) * 100 * (hsv1[1] / 100) * (hsv1[2] / 100) - Math.sin(Math.radians(hsv2[0])) * 100 * (hsv2[1]) / 100 * (hsv2[2]) / 100) ** 2) +
-    ((Math.cos(Math.radians(hsv1[0])) * 100 * (hsv1[1] / 100) * (hsv1[2] / 100) - Math.cos(Math.radians(hsv2[0])) * 100 * (hsv2[1]) / 100 * (hsv2[2]) / 100) ** 2) +
-    ((hsv1[2] - hsv2[2]) ** 2)
-)
+const rgbToHex = (r, g, b) => {
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
 
-const compute = hsv => colors
-    .map(c => ((c.distance = hsvDistance(hsv, c.hsv)) && c) || c)
+const distance = (rgb1, rgb2) => {
+    if (typeof rgb1 == 'string') rgb1 = rgb1.split(/, */g).map(c => parseInt(c))
+    if (typeof rgb2 == 'string') rgb2 = rgb2.split(/, */g).map(c => parseInt(c))
+    if (rgb1.r) rgb1 = [rgb1.r, rgb1.g, rgb1.b]
+    if (rgb2.r) rgb2 = [rgb2.r, rgb2.g, rgb2.b]
+    return ((rgb2[0] - rgb1[0])**2 + (rgb2[1] - rgb1[1])**2 + (rgb2[2] - rgb1[2])**2)**0.5
+}
+
+const compute = rgb => colors
+    .map(c => ((c.distance = distance(rgb, c.rgb)) && c) || c)
     .sort((c1, c2) => c1.distance < c2.distance ? -1 : 1)
 
 module.exports = {
-    "hsvColorName": (h, s, v) => {
-        assert(0 <= h && h <= 360, 'Hue should be between 0 and 360')
-        assert(0 <= s && s <= 100, 'Saturation should be between 0 and 100')
-        assert(0 <= v && v <= 100, 'Vibrance/Brightness should be between 0 and 100')
-        return compute([h, s, v])[0]
-    },
     "rgbColorName": (r, g, b) => {
         assert(0 <= r && r <= 255, "Red channel should be 0-255")
         assert(0 <= g && g <= 255, "Green channel should be 0-255")
         assert(0 <= b && b <= 255, "Blue channel should be 0-255")
-        return compute(rgb_to_hsv(r, g, b))[0]
+        let nearestColor = compute([r, g, b])[0]
+        return nearestColor.distance < 40 ? nearestColor : {
+            "name": `(${r}, ${g}, ${b})`,
+            "hex": rgbToHex(r,g,b),
+            "rgb": `${r},${g},${b}`
+        }
     },
     "hexColorName": hex => {
         assert(typeof hex === 'string', 'Hex should be a string.')
         if (hex.startsWith('#')) hex = hex.slice(1)
         assert(hex.length == 6, "Requires 6 digit hex code")
-        return compute(rgb_to_hsv(...hex_to_rgb(hex)))[0]
+        let nearestColor = compute(hex_to_rgb(hex))[0]
+        return nearestColor.distance < 40 ? nearestColor : {
+            "name": '#' + hex,
+            "hex": hex,
+            "rgb": hex_to_rgb(hex).join(',')
+        }
     },
-    "hsvDistance": hsvDistance,
+    "distance": distance,
     "compute": compute,
-    "converters": {
-        "rgb_to_hsv": rgb_to_hsv,
-        "hex_to_rgb": hex_to_rgb
-    }
+    "hex_to_rgb": hex_to_rgb
 }
